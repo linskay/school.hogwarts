d4856180ae03b87288605f492c7a6f4b059acea2
Lins <alina.cheremiskina@gmail.com>
2024-11-22 21:07:41 +0300

    4.2 complete

--- src/main/java/ru/hogwarts/school/scripts/scripts422.sql
+++ src/main/java/ru/hogwarts/school/scripts/scripts422.sql
@@ -1,2 +1,13 @@
-select *
-from student;
+CREATE TABLE car
+(
+    Id    SERIAL PRIMARY KEY,
+    Make  varchar NOT NULL,
+    Model VARCHAR NOT NULL,
+    Price INTEGER check (Price > 0)
+);
+CREATE TABLE people
+(
+    Name    varchar PRIMARY KEY,
+    Age     INTEGER CHECK (Age > 0),
+    License BOOLEAN
+);
--- src/main/java/ru/hogwarts/school/scripts/scripts423.sql
+++ src/main/java/ru/hogwarts/school/scripts/scripts423.sql
@@ -1,2 +1,6 @@
-select *
-from student;
+SELECT student.name, student.age, faculty.name
+FROM student
+         INNER JOIN faculty ON student.faculty_id = faculty.id;
+SELECT student.name
+FROM student
+         INNER JOIN avatar ON student.id = student_id;
